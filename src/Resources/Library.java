9d75ecc2767ac68cb21137f9dcef9c4c648ecf62
Cumali Badur <cumalibadur@gmail.com>
2020-04-23 10:38:12 -0400

    Day 27

--- src/Resources/Library.java
+++ src/Resources/Library.java
@@ -77,6 +77,18 @@ public class Library {
         return result;
     }
 
+    public static int frequency(String str, char ch){ // counts the ch' frequency
+        char[] arr =  str.toCharArray(); // [A, B, B]
+
+        int count = 0; //1
+        for(char each  : arr ){ //3   each: A , B, B
+            if(each == ch){
+                count++;
+            }
+        }
+
+        return count;
+    }
 
 
 
