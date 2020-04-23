67cee79c2033537deeb31053b15bc0475dac35d3
Cumali Badur <cumalibadur@gmail.com>
2020-04-23 10:59:12 -0400

    Day 27

--- src/day27_DateTime/unique_Arrays.java
+++ src/day27_DateTime/unique_Arrays.java
@@ -3,10 +3,25 @@ package day27_DateTime;
 public class unique_Arrays {
 
     public static void main(String[] args) {
+
+
+
 int []arr={1,1,2,3,3};
+
+for(int each2:arr){
+
+}
+
 int count =0;
 for(int each:arr){
 
-}
+    if(each==arr[0]){
+        count++;
+        if(count==1){
+
     }
+} System.out.println(arr[2]);
+
 }
+    }}
+
